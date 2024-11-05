335ea80174d7e34c8217257af1bee0b0b1dbdb46
Euler Alvarenga <euler.alvarenga.dev@gmail.com>
2024-11-05 17:01:31 -0300

    basic card component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,27 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ScrollView } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import { Card } from "@/components/Card";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <ScrollView style={{ width: "100%" }}>
+        <View style={{ gap: 8 }}>
+          <Card name="Euler" bgColor="#1abc9c" score={0} />
+          <Card name="Ingrid" bgColor="#3498db" score={10} />
+          <Card name="Eydde" bgColor="#8e44ad" score={7} />
+          <Card name="Mario" bgColor="#e67e22" score={9} />
+        </View>
+      </ScrollView>
+      {/* <Text style={styles.title}>Tab One</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
     </View>
   );
 }
@@ -16,16 +29,19 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    // justifyContent: "center",
+    // borderColor: "red",
+    // borderWidth: 1,
+    padding: 8,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
---
+++ components/Card.tsx
@@ -0,0 +1,93 @@
+import { StyleSheet, Pressable } from "react-native";
+import { Text, View } from "./Themed";
+import Octicons from "@expo/vector-icons/Octicons";
+
+type CardProps = {
+  name: string;
+  score: number;
+  bgColor: string;
+};
+export function Card({ name, score = 0, bgColor }: CardProps) {
+  return (
+    <View style={[styles.container, { backgroundColor: bgColor }]}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          backgroundColor: "transparent",
+        }}
+      >
+        <Text style={styles.title}>{name}</Text>
+        <Pressable
+          onPress={() => {
+            console.log("press edit");
+          }}
+        >
+          <Octicons
+            size={16}
+            // style={{ marginBottom: -3 }}
+            name="pencil"
+            color="white"
+          />
+        </Pressable>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          paddingHorizontal: 30,
+          paddingVertical: 20,
+          backgroundColor: "transparent",
+        }}
+      >
+        <Pressable
+          onPress={() => {
+            console.log("press minus");
+          }}
+        >
+          <Octicons
+            size={32}
+            // style={{ marginBottom: -3 }}
+            name="dash"
+            color="white"
+          />
+        </Pressable>
+        <Text style={styles.score}>{score}</Text>
+        <Pressable
+          onPress={() => {
+            console.log("press plus");
+          }}
+        >
+          <Octicons
+            size={32}
+            // style={{ marginBottom: -3 }}
+            name="plus"
+            color="white"
+          />
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: 120,
+
+    borderRadius: 8,
+    padding: 16,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
+  },
+  score: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
